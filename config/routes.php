4f34e153247cb75f8986f4d2ec2a102a7f52e999
Nicholas Leite <nicklleite@gmail.com>
2024-08-01 15:52:14 -0300

    Prefix the resource routes for the API; CRUD actions for the Posts endpoint; Dependencies update

--- config/routes.php
+++ config/routes.php
@@ -49,39 +49,39 @@ return function (RouteBuilder $routes): void {
      */
     $routes->setRouteClass(DashedRoute::class);
 
-    $routes->scope('/api', function ($routes) {
+    $routes->prefix('Api', function (RouteBuilder $routes): void {
         $routes->setExtensions(['json']);
         $routes->resources('Posts');
     });
 
-    $routes->scope('/', function (RouteBuilder $builder): void {
-        /*
-         * Here, we are connecting '/' (base path) to a controller called 'Pages',
-         * its action called 'display', and we pass a param to select the view file
-         * to use (in this case, templates/Pages/home.php)...
-         */
-        $builder->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
+    // $routes->scope('/', function (RouteBuilder $builder): void {
+    //     /*
+    //      * Here, we are connecting '/' (base path) to a controller called 'Pages',
+    //      * its action called 'display', and we pass a param to select the view file
+    //      * to use (in this case, templates/Pages/home.php)...
+    //      */
+    //     $builder->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
 
-        /*
-         * ...and connect the rest of 'Pages' controller's URLs.
-         */
-        $builder->connect('/pages/*', 'Pages::display');
+    //     /*
+    //      * ...and connect the rest of 'Pages' controller's URLs.
+    //      */
+    //     $builder->connect('/pages/*', 'Pages::display');
 
-        /*
-         * Connect catchall routes for all controllers.
-         *
-         * The `fallbacks` method is a shortcut for
-         *
-         * ```
-         * $builder->connect('/{controller}', ['action' => 'index']);
-         * $builder->connect('/{controller}/{action}/*', []);
-         * ```
-         *
-         * You can remove these routes once you've connected the
-         * routes you want in your application.
-         */
-        $builder->fallbacks();
-    });
+    //     /*
+    //      * Connect catchall routes for all controllers.
+    //      *
+    //      * The `fallbacks` method is a shortcut for
+    //      *
+    //      * ```
+    //      * $builder->connect('/{controller}', ['action' => 'index']);
+    //      * $builder->connect('/{controller}/{action}/*', []);
+    //      * ```
+    //      *
+    //      * You can remove these routes once you've connected the
+    //      * routes you want in your application.
+    //      */
+    //     $builder->fallbacks();
+    // });
 
     /*
      * If you need a different set of middleware or none at all,
---
+++ src/Controller/Api/PostsController.php
@@ -0,0 +1,77 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller\Api;
+use App\Controller\AppController;
+use Cake\View\JsonView;
+
+/**
+ * Posts Controller
+ *
+ * @property \App\Model\Table\PostsTable $Posts
+ */
+class PostsController extends AppController
+{
+
+    public function viewClasses(): array
+    {
+        return [JsonView::class];
+    }
+
+    public function index()
+    {
+        $posts = $this->Posts->find('all')->all();
+        $this->set('posts', $posts);
+        $this->viewBuilder()->setOption('serialize', ['posts']);
+    }
+
+    public function view($id)
+    {
+        $post = $this->Posts->get($id);
+        $this->set('post', $post);
+        $this->viewBuilder()->setOption('serialize', ['post']);
+    }
+
+    public function add()
+    {
+        $post = $this->Posts->newEmptyEntity();
+        $isRequestMethodPost = $this->request->is('post');
+
+        if ($isRequestMethodPost) {
+            $post = $this->Posts->patchEntity($post, $this->request->getData());
+
+            if ($this->Posts->save($post)) {
+                $this->set('post', $post);
+                $this->viewBuilder()->setOption('serialize', ['post']);
+            }
+        }
+    }
+
+    public function edit($id)
+    {
+        $post = $this->Posts->get($id);
+        $isRequestMethodPutOrPatch = $this->request->is(['put', 'patch']);
+
+        if ($isRequestMethodPutOrPatch) {
+            $post = $this->Posts->patchEntity($post, $this->request->getData());
+
+            if ($this->Posts->save($post)) {
+                $this->set('post', $post);
+                $this->viewBuilder()->setOption('serialize', ['post']);
+            }
+        }
+    }
+
+    public function delete($id)
+    {
+        $post = $this->Posts->get($id);
+        $isRequestMethodDelete = $this->request->is('delete');
+
+        if ($isRequestMethodDelete) {
+            if ($this->Posts->delete($post)) {
+                $this->set('post', $post);
+                $this->viewBuilder()->setOption('serialize', ['post']);
+            }
+        }
+    }
+}
